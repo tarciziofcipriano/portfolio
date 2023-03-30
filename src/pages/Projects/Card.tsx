90718d0701d8778dbfd276dc5b38b31ed94f669c
LAPTOP-BEA7D8KF\tarci <tarcizio.cipriano@aticks.com>
2023-03-30 17:41:58 -0300

    Fix hover nos cards dos projetos

--- src/pages/Projects/Card.tsx
+++ src/pages/Projects/Card.tsx
@@ -1,6 +1,5 @@
 import {
   Card as MuiCard,
-  CardActionArea,
   CardContent,
   CardMedia,
   Typography,
@@ -15,34 +14,30 @@ interface CardDataProps {
 
 const Card = ({ title, description, link, image }: CardDataProps) => {
   return (
-    <MuiCard sx={{ height: 300, width: 270, m: 2, mt: 0, mb: 4 }}>
-      <a
-        href={link}
-        target="_blank"
-        rel="noreferrer"
-        style={{
-          color: "inherit",
-          textDecoration: "none",
-          display: "block",
-        }}
-      >
-        <CardActionArea>
-          <CardMedia
-            component="img"
-            height="140"
-            image={image}
-            alt="green iguana"
-          />
-          <CardContent>
-            <Typography gutterBottom fontSize={20} component="div">
-              {title}
-            </Typography>
-            <Typography variant="body2" color="text.secondary">
-              {description}
-            </Typography>
-          </CardContent>
-        </CardActionArea>
-      </a>
+    <MuiCard
+      sx={{
+        height: 300,
+        width: 270,
+        m: 2,
+        mt: 0,
+        mb: 4,
+        "&:hover": {
+          backgroundColor: "#F6F6F6",
+          boxShadow:
+            "rgba(0, 0, 0, 0.25) 0px 14px 28px, rgba(0, 0, 0, 0.22) 0px 10px 10px",
+          cursor: "pointer",
+        },
+      }}
+    >
+      <CardMedia component="img" height="140" image={image} />
+      <CardContent>
+        <Typography gutterBottom fontSize={20} component="div">
+          {title}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          {description}
+        </Typography>
+      </CardContent>
     </MuiCard>
   );
 };
