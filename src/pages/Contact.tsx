fa75feedf3ba1a95630b2abc13bb6032aa380042
LAPTOP-BEA7D8KF\tarci <tarcizio.cipriano@aticks.com>
2023-04-03 16:36:12 -0300

    Fix interface da página Contato

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -3,7 +3,13 @@ import emailjs from "@emailjs/browser";
 
 import { useTranslation } from "react-i18next";
 
-import { ContactMail as ContactMailIcon } from "@mui/icons-material";
+import {
+  ChatBubbleOutline as ChatBubbleOutlineIcon,
+  ContactMail as ContactMailIcon,
+  MailOutline as MailOutlineIcon,
+  PersonOutline as PersonOutlineIcon,
+  Send as SendIcon,
+} from "@mui/icons-material";
 import {
   Alert,
   Box,
@@ -13,11 +19,21 @@ import {
   Slide,
   TextField,
   Typography,
+  styled,
 } from "@mui/material";
 import { ToastContext } from "../context/ToastContext";
+import useIsMobile from "../hooks/useIsMobile";
+
+const CustomTextField = styled(TextField)`
+  fieldset {
+    border-radius: 20px;
+    border: 1px solid #b2b9c2;
+  }
+`;
 
 const Contact = () => {
   const { t } = useTranslation();
+  const mobile = useIsMobile();
   const { toastSuccess, toastError } = React.useContext(ToastContext);
   const [name, setName] = React.useState("");
   const [email, setEmail] = React.useState("");
@@ -69,32 +85,72 @@ const Contact = () => {
               {t("TEXT.CONTACT")}
             </Typography>
           </Box>
-          <Paper sx={{ p: 4, borderRadius: 3 }} elevation={3}>
+          <Paper
+            sx={{ p: 4, pr: mobile ? 3 : 4, pl: 2, borderRadius: 3 }}
+            elevation={3}
+          >
             <form ref={form} onSubmit={sendEmail}>
               <Box display="grid">
-                <TextField
-                  label={t("TEXT.NAME")}
-                  name="name"
-                  onChange={(e) => handleChangeName(e.target.value)}
-                />
-                <TextField
-                  label="Email"
-                  name="email"
-                  onChange={(e) => handleChangeEmail(e.target.value)}
-                  sx={{ mt: 2, mb: 2 }}
-                />
-                <TextField
-                  label={t("TEXT.MESSAGE")}
-                  name="message"
-                  multiline
-                  rows={6}
-                  onChange={(e) => handleChangeMessage(e.target.value)}
-                  sx={{ mb: 2 }}
-                />
+                <Box sx={{ display: "flex", alignItems: "flex-end" }}>
+                  <PersonOutlineIcon
+                    sx={{
+                      mr: 1,
+                      color: "action.active",
+                      alignSelf: "center",
+                    }}
+                    fontSize="medium"
+                  />
+                  <CustomTextField
+                    label={t("TEXT.NAME")}
+                    name="name"
+                    color="primary"
+                    onChange={(e) => handleChangeName(e.target.value)}
+                    sx={{ width: "100%" }}
+                  />
+                </Box>
+                <Box sx={{ display: "flex", alignItems: "flex-end" }}>
+                  <MailOutlineIcon
+                    sx={{
+                      mr: 1,
+                      color: "action.active",
+                      alignSelf: "center",
+                    }}
+                    fontSize="medium"
+                  />
+                  <CustomTextField
+                    label="Email"
+                    name="email"
+                    color="primary"
+                    onChange={(e) => handleChangeEmail(e.target.value)}
+                    sx={{ mt: 2, mb: 2, width: "100%" }}
+                  />
+                </Box>
+                <Box sx={{ display: "flex", alignItems: "flex-end" }}>
+                  <ChatBubbleOutlineIcon
+                    sx={{
+                      mt: 0,
+                      mr: 1,
+                      mb: 2,
+                      color: "action.active",
+                      alignSelf: "center",
+                    }}
+                    fontSize="medium"
+                  />
+                  <CustomTextField
+                    label={t("TEXT.MESSAGE")}
+                    name="message"
+                    multiline
+                    rows={6}
+                    color="primary"
+                    onChange={(e) => handleChangeMessage(e.target.value)}
+                    sx={{ mb: 2, width: "100%" }}
+                  />
+                </Box>
               </Box>
               <Button
                 disabled={!isFormValid}
                 variant="contained"
+                endIcon={<SendIcon />}
                 type="submit"
                 sx={{ width: 120 }}
               >
